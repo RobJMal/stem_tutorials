59e1f9ab1907d2357d5dd588fe10b5baf385ab1c
RobJMal <robjmal@gmail.com>
2021-10-16 22:35:02 -0700

    Added more information about the axes

--- rotation_matrix_tutorial/main.py
+++ rotation_matrix_tutorial/main.py
@@ -4,7 +4,7 @@ class XHatVectorDerivation(Scene):
     def construct(self):
         self.show_circle()
         self.show_circle_axis()
-        self.show_x_hat_axis()
+        self.show_x_coodinate_axis()
         self.show_y_hat_axis()
 
     def show_circle_axis(self): 
@@ -29,31 +29,56 @@ class XHatVectorDerivation(Scene):
         self.add(circle)
         self.circle = circle
 
-    def show_x_hat_axis(self):
-        self.x_hat_origin_point = np.array([0, 2, 0])
+    def show_x_coodinate_axis(self):
+        self.x_coodinate_origin_point = np.array([-1, 2, 0])
 
-        x_start = self.x_hat_origin_point + np.array([-1, 0, 0])
-        x_end = self.x_hat_origin_point + np.array([6, 0, 0])
+        x_start = self.x_coodinate_origin_point + np.array([-1, 0, 0])
+        x_end = self.x_coodinate_origin_point + np.array([7, 0, 0])
 
-        y_start = self.x_hat_origin_point + np.array([0, 1.5, 0])
-        y_end = self.x_hat_origin_point + np.array([0, -1.5, 0])
+        y_start = self.x_coodinate_origin_point + np.array([0, 1.5, 0])
+        y_end = self.x_coodinate_origin_point + np.array([0, -1.5, 0])
 
         x_axis = Line(x_start, x_end)
         y_axis = Line(y_start, y_end)
 
         self.add(x_axis, y_axis)
+        self.add_hat_axes_labels(self.x_coodinate_origin_point)
 
     def show_y_hat_axis(self):
-        self.y_hat_origin_point = np.array([0, -2, 0])
+        self.y_coordinate_origin_point = np.array([-1, -2, 0])
 
-        x_start = self.y_hat_origin_point + np.array([-1, 0, 0])
-        x_end = self.y_hat_origin_point + np.array([6, 0, 0])
+        x_start = self.y_coordinate_origin_point + np.array([-1, 0, 0])
+        x_end = self.y_coordinate_origin_point + np.array([7, 0, 0])
 
-        y_start = self.y_hat_origin_point + np.array([0, 1.5, 0])
-        y_end = self.y_hat_origin_point + np.array([0, -1.5, 0])
+        y_start = self.y_coordinate_origin_point + np.array([0, 1.5, 0])
+        y_end = self.y_coordinate_origin_point + np.array([0, -1.5, 0])
 
         x_axis = Line(x_start, x_end)
         y_axis = Line(y_start, y_end)
 
         self.add(x_axis, y_axis)
+        self.add_hat_axes_labels(self.y_coordinate_origin_point)
+
+    # Adds the labels for the x_hat and y_hat axes
+    def add_hat_axes_labels(self, axes_origin_point):
+        x_labels = [
+            MathTex(r"\frac{\pi}{2}"), MathTex("\pi"),
+            MathTex(r"\frac{3 \pi}{2}"), MathTex("2 \pi")
+        ]
+
+        y_labels = [MathTex("1"), MathTex("-1")]
+
+        for i in range(len(x_labels)):
+            x_labels[i].next_to(axes_origin_point + np.array([PI*(i+1)/2, 0, 0]), DOWN)
+            self.add(x_labels[i])
+        
+        y_labels[0].next_to(axes_origin_point + np.array([0, 1, 0]), LEFT)
+        y_labels[1].next_to(axes_origin_point + np.array([0, -1, 0]), LEFT)
+        self.add(y_labels[0], y_labels[1])
+
+        # for i in range(len(y_labels)):
+        #     y_labels[i].next_to(axes_origin_point + np.array([0, (i*-1)+2, 0]), RIGHT)
+        #     self.add(y_labels[i])
+
+
 
